b5f6a742aea73d934baed781d7184bfb61bcd013
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-06-21 17:40:59 +0100

    Bump to v3.1.1

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule SipHash.Mixfile do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: "3.1.0",
+      version: "3.1.1",
       elixir: "~> 1.1",
       aliases: [
         clean: [ "clean", "clean.make" ]
