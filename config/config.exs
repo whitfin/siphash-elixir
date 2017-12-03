51b7ded206330335eb0a04c0e3cb60437baa2a6f
Isaac Whitfield <iw@whitfin.io>
2017-12-02 17:01:52 -0800

    Revert accidental configuration override

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ use Mix.Config
 #     config :logger, level: :info
 #
 config :siphash,
-  disable_nifs: true
+  disable_nifs: System.get_env("SIPHASH_IMPL") == "embedded"
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
